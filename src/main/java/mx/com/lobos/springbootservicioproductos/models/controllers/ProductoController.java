e8d8564a714df570ff096298265a4fb2b2c9afab
chavonac <chavonbassguitar@hotmail.com>
2019-12-25 17:49:51 -0600

    Curso microservicios Seccion 3 capitulo 23

--- src/main/java/mx/com/lobos/springbootservicioproductos/models/controllers/ProductoController.java
+++ src/main/java/mx/com/lobos/springbootservicioproductos/models/controllers/ProductoController.java
@@ -6,9 +6,13 @@
 package mx.com.lobos.springbootservicioproductos.models.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import mx.com.lobos.springbootservicioproductos.models.entity.Producto;
 import mx.com.lobos.springbootservicioproductos.models.service.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,18 +23,31 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ProductoController {
+	
+	@Autowired
+	private Environment env;
 
+	@Value("${server.port}")
+	private Integer port;
+	
     @Autowired
     private IProductoService productoService;
 
     @GetMapping("/listar")
     public List<Producto> listar() {
-        return productoService.findAll();
+        return productoService.findAll().stream().map(producto -> {
+//        	producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+        	producto.setPort(port);
+        	return producto;
+        }).collect(Collectors.toList());
     }
 
     @GetMapping("/ver/{id}")
     public Producto detalle(@PathVariable Long id) {
-        return productoService.findById(id);
+    	Producto producto = productoService.findById(id);
+//    	producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+    	producto.setPort(port);
+    	return productoService.findById(id);
     }
 
 }
--- src/main/java/mx/com/lobos/springbootservicioproductos/models/entity/Producto.java
+++ src/main/java/mx/com/lobos/springbootservicioproductos/models/entity/Producto.java
@@ -10,54 +10,66 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "productos")
 public class Producto implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String nombre;
-    
-    private Double precio;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(name = "creat_at")
-    @Temporal(TemporalType.DATE)
-    private Date createAt;
+	private String nombre;
 
-    public Long getId() {
-        return this.id;
-    }
+	private Double precio;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
 
-    public String getNombre() {
-        return this.nombre;
-    }
+	@Transient
+	private Integer port;
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public Long getId() {
+		return this.id;
+	}
 
-    public Double getPrecio() {
-        return this.precio;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
+	public String getNombre() {
+		return this.nombre;
+	}
 
-    public Date getCreateAt() {
-        return this.createAt;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
+	public Double getPrecio() {
+		return this.precio;
+	}
 
-    private static final long serialVerionUID = 128545430635845809L;
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Date getCreateAt() {
+		return this.createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+
+	private static final long serialVersionUID = 540735049209185036L;
 }
